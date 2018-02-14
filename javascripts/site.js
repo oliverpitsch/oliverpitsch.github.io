c47c9c64e3e9a858a097822c6408031fe5d1dd61
Oliver Pitsch <oliver.pitsch@trustedshops.de>
2018-02-14 23:58:04 +0100

    New Site Baby

---
+++ javascripts/site.js
@@ -0,0 +1,11 @@
+$( document ).ready(function(){
+  // var swiper = new Swiper('.swiper-container', {
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
+
+  var currentAgeSpan = $('.about-me--text').find('span.age');
+  console.log(currentAgeSpan);
+});
