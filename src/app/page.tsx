49270961311a08bfff04c7fee2fda5f5040c1ce2
Oliver Pitsch <oliver@pitsch.me>
2025-09-16 11:07:00 +0200

    Add waving hand emoji to the homepage message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-amber-100">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl sm:text-5xl font-semibold tracking-[-.02em] text-center sm:text-left text-amber-600">
-        Pitsch.me will be right back.
+        Pitsch.me will be right back. 👋
         </h1>
           
       </main>
