3c1ae30516d96afaba250c88c925541062be6c15
Oliver Pitsch <oliver@pitsch.me>
2025-09-16 11:04:34 +0200

    Refactor code structure for improved readability and maintainability

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xcY5oUTBjlm971R562o4NdxK8PQC52ZlQUMRbESCCbY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
